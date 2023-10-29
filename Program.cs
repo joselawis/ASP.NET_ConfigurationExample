c0542b6cabb7cc6cd30739864910a4c7bf45b81e
joseluiscontreras <joseluis.contreras@innocv.com>
2023-10-29 18:45:32 +0100

    custom json configuration

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.Configure<WeatherApiOptions>(builder.Configuration.GetSection("weatherApi"));
 
+// Load MyOwnConfig.json
+builder.Configuration.AddJsonFile("MyOwnConfig.json", true, true);
 var app = builder.Build();
 
 app.UseStaticFiles();
